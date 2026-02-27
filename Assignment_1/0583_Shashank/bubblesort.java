b6cf1548280bbddd565566f2fcd592cd8c1b903a
ShashankChary <23ag1a0583@gmail.com>
2026-02-27 12:20:15 +0530

    assignmentsubmitted

---
+++ Assignment_1/0583_Shashank/bubblesort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class BubbleSortExample {
+
+    // Function to perform bubble sort
+    static void bubbleSort(int arr[], int n) {
+        int temp;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    // Swap
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Function to print array
+    static void printArray(int arr[], int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of elements:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr, n);
+
+        System.out.println("Sorted array:");
+        printArray(arr, n);
+    }
+}
